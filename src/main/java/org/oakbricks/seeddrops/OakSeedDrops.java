0a39dfa4c5f0c986457a51d93b5a3b1c32bca9b6
OakBricks <oakbricks@gmail.com>
2021-08-09 14:01:07 -0500

    when the 1.17.x

--- src/main/java/org/oakbricks/seeddrops/OakSeedDrops.java
+++ src/main/java/org/oakbricks/seeddrops/OakSeedDrops.java
@@ -1,62 +1,81 @@
 package org.oakbricks.seeddrops;
 
+import net.devtech.arrp.api.RRPCallback;
+import net.devtech.arrp.api.RuntimeResourcePack;
+
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+
 import net.minecraft.block.Blocks;
-import net.minecraft.item.Items;
-import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.util.Identifier;
-import net.minecraft.loot.ConstantLootTableRange;
-import net.minecraft.loot.entry.ItemEntry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+
+import static net.devtech.arrp.api.RuntimeResourcePack.id;
+import static net.devtech.arrp.json.loot.JLootTable.*;
 
 public class OakSeedDrops implements ModInitializer {
 	
-	public static final OakSeedDropsConfig CONFIG = AutoConfig.register(OakSeedDropsConfig.class, Toml4jConfigSerializer::new).getConfig();
+	public static final OakSeedDropsConfig CONFIG = AutoConfig.register(OakSeedDropsConfig.class, JanksonConfigSerializer::new).getConfig();
 	public static final String MOD_ID = "oak_seed_drops";
 	public static final Logger LOGGER = LogManager.getLogger("OakBricks' Seed Drops Mod");
-	
+	public static final Identifier GRASS_LOOT_TABLE_ID = Blocks.GRASS.getLootTableId();
+	public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create(MOD_ID + ":drops");
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Carrot Drop Rolls: " + CONFIG.drops.carrot_rolls);
-		LOGGER.info("Potato Drop Rolls: " + CONFIG.drops.potato_rolls);
-		LOGGER.info("Beetroot Seeds Drop Rolls: " + CONFIG.drops.beetroot_seeds_rolls);
-		LOGGER.info("Melon Seeds Drop Rolls: " + CONFIG.drops.melon_seeds_rolls);
-		LOGGER.info("Pumpkin Seeds Drop Rolls: " + CONFIG.drops.pumpkin_seeds_rolls);
+		LOGGER.debug("Carrot Drop Rolls: " + CONFIG.rolls.carrot_rolls);
+		LOGGER.debug("Potato Drop Rolls: " + CONFIG.rolls.potato_rolls);
+		LOGGER.debug("Beetroot Seeds Drop Rolls: " + CONFIG.rolls.beetroot_seeds_rolls);
+		LOGGER.debug("Melon Seeds Drop Rolls: " + CONFIG.rolls.melon_seeds_rolls);
+		LOGGER.debug("Pumpkin Seeds Drop Rolls: " + CONFIG.rolls.pumpkin_seeds_rolls);
 
-		/*private static */final Identifier GRASS_LOOT_TABLE_ID = Blocks.GRASS.getLootTableId();
 
-		LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, id, table, setter) -> {
-		   	if (GRASS_LOOT_TABLE_ID.equals(id)) {
-		       	if (GRASS_LOOT_TABLE_ID.equals(id)) {
-		           	FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-		           			.rolls(ConstantLootTableRange.create(CONFIG.drops.carrot_rolls))
-		           			.with(ItemEntry.builder(Items.CARROT))
-		           			.rolls(ConstantLootTableRange.create(CONFIG.drops.melon_seeds_rolls))
-		           			.with(ItemEntry.builder(Items.MELON_SEEDS))
-		           			.rolls(ConstantLootTableRange.create(CONFIG.drops.pumpkin_seeds_rolls))
-		           			.with(ItemEntry.builder(Items.PUMPKIN_SEEDS))
-		           			.rolls(ConstantLootTableRange.create(CONFIG.drops.beetroot_seeds_rolls))
-		           			.with(ItemEntry.builder(Items.BEETROOT_SEEDS))
-		           			.rolls(ConstantLootTableRange.create(CONFIG.drops.wheat_seeds_booster_rolls))
-		           			.with(ItemEntry.builder(Items.WHEAT_SEEDS))
-		           			.rolls(ConstantLootTableRange.create(CONFIG.drops.potato_rolls))
-		           			.with(ItemEntry.builder(Items.POTATO));
-		           
-		           	table.pool(poolBuilder);
-		        }
-		    }
-		
-		}));
+//		LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, id, table, setter) -> {
+//		   	if (GRASS_LOOT_TABLE_ID.equals(id)) {
+//		       	if (GRASS_LOOT_TABLE_ID.equals(id)) {
+//		           	FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+//		           			.rolls(ConstantLootTableRange.create(CONFIG.rolls.carrot_rolls))
+//		           			.with(ItemEntry.builder(Items.CARROT))
+//		           			.rolls(ConstantLootTableRange.create(CONFIG.rolls.melon_seeds_rolls))
+//		           			.with(ItemEntry.builder(Items.MELON_SEEDS))
+//		           			.rolls(ConstantLootTableRange.create(CONFIG.rolls.pumpkin_seeds_rolls))
+//		           			.with(ItemEntry.builder(Items.PUMPKIN_SEEDS))
+//		           			.rolls(ConstantLootTableRange.create(CONFIG.rolls.beetroot_seeds_rolls))
+//		           			.with(ItemEntry.builder(Items.BEETROOT_SEEDS))
+//		           			.rolls(ConstantLootTableRange.create(CONFIG.rolls.wheat_seeds_booster_rolls))
+//		           			.with(ItemEntry.builder(Items.WHEAT_SEEDS))
+//		           			.rolls(ConstantLootTableRange.(CONFIG.rolls.potato_rolls))
+//		           			.with(ItemEntry.builder(Items.POTATO));
+//
+//		           	table.pool(poolBuilder);
+//		        }
+//		    }
+//
+//		}));
+		RESOURCE_PACK.addLootTable(id("minecraft:blocks/grass"),
+				loot("minecraft:block")
+						.pool(pool()
+								.rolls(CONFIG.rolls.carrot_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:carrot")
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.carrot_chances)))
+								.rolls(CONFIG.rolls.potato_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:potato")
+								)
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.potato_chances)))
+		);
+		RRPCallback.BEFORE_VANILLA.register(a -> a.add(RESOURCE_PACK));
 	}
 }
--- src/main/java/org/oakbricks/seeddrops/OakSeedDropsConfig.java
+++ src/main/java/org/oakbricks/seeddrops/OakSeedDropsConfig.java
@@ -3,19 +3,28 @@ package org.oakbricks.seeddrops;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import java.lang.Number;
 
 @Config(name = OakSeedDrops.MOD_ID)
 class OakSeedDropsConfig implements ConfigData {
-    
     @ConfigEntry.Gui.CollapsibleObject
-    InnerStuff drops = new InnerStuff();
+    Rolls rolls = new Rolls();
+    Chances chances = new Chances();
     
-    static class InnerStuff {
+    static class Rolls {
         int carrot_rolls = 0;
         int potato_rolls = 0;
         int beetroot_seeds_rolls = 0;
         int pumpkin_seeds_rolls = 0;
         int melon_seeds_rolls = 0;
         int wheat_seeds_booster_rolls = 0;
+    }
+    static class Chances {
+        Number carrot_chances = 0;
+        Number potato_chances = 0;
+        Number beetroot_seeds_chances = 0;
+        Number pumpkin_seeds_chances = 0;
+        Number melon_seeds_chances = 0;
+        Number wheat_seeds_booster_chances = 0;
     }
 }
