7da2b199501e0cf39c802625159f533a14e51d75
OakBricks <andycruzfranco@gmail.com>
2021-09-25 16:20:01 -0500

    I think it works now

--- src/main/java/org/oakbricks/seeddrops/OakSeedDrops.java
+++ src/main/java/org/oakbricks/seeddrops/OakSeedDrops.java
@@ -2,23 +2,71 @@ package org.oakbricks.seeddrops;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import net.devtech.arrp.api.RRPCallback;
 import net.devtech.arrp.api.RuntimeResourcePack;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import static net.devtech.arrp.api.RuntimeResourcePack.id;
+import static net.devtech.arrp.json.loot.JLootTable.*;
 
 public class OakSeedDrops implements ModInitializer {
 	
 	public static final OakSeedDropsConfig CONFIG = AutoConfig.register(OakSeedDropsConfig.class, JanksonConfigSerializer::new).getConfig();
 	public static final String MOD_ID = "oak_seed_drops";
 	public static final Logger LOGGER = LogManager.getLogger("OakBricks' Seed Drops Mod");
-	public static final Identifier GRASS_LOOT_TABLE_ID = Blocks.GRASS.getLootTableId();
 	public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create(MOD_ID + ":drops");
 
 	@Override
 	public void onInitialize() {
+		RESOURCE_PACK.addLootTable(id("minecraft:blocks/grass"),
+				loot("minecraft:block")
+						.pool(pool()
+								.rolls(CONFIG.rolls.carrot_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:carrot"))
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.carrot_chances)))
+		);
+		RESOURCE_PACK.addLootTable(id("minecraft:blocks/grass"),
+				loot("minecraft:block")
+						.pool(pool()
+								.rolls(CONFIG.rolls.potato_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:potato"))
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.potato_chances)))
+		);
+		RESOURCE_PACK.addLootTable(id("minecraft:blocks/grass"),
+				loot("minecraft:block")
+						.pool(pool()
+								.rolls(CONFIG.rolls.beetroot_seeds_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:beetroot_seeds"))
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.beetroot_seeds_chances)))
+		);
+		RESOURCE_PACK.addLootTable(id("minecraft:blocks/grass"),
+				loot("minecraft:block")
+						.pool(pool()
+								.rolls(CONFIG.rolls.melon_seeds_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:melon_seeds"))
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.melon_seeds_chances)))
+
+		);
+		RESOURCE_PACK.addLootTable(id("minecraft:blocks/grass"),
+				loot("minecraft:block")
+						.pool(pool()
+								.rolls(CONFIG.rolls.pumpkin_seeds_rolls)
+								.entry(entry()
+										.type("minecraft:item")
+										.name("minecraft:pumpkin_seeds"))
+								.condition(predicate("minecraft:random_chance").parameter("chance", CONFIG.chances.pumpkin_seeds_chances)))
+		);
+		RRPCallback.AFTER_VANILLA.register(a -> a.add(RESOURCE_PACK));
 		LOGGER.info("nuts nuts");
 	}
 }
